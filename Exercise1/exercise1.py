d825a134c289d18d8405491457082f5de2374cc2
Valerie <v.bartel.vb@gmail.com>
2024-04-23 16:15:08 +0200

    start exercise 3

--- Exercise1/exercise1.py
+++ Exercise1/exercise1.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import sounddevice as sd
 import numpy as np
 from numpy.lib.stride_tricks import sliding_window_view
+from scipy.signal import find_peaks
 
 # Exercise 1.1
 # data, samplerate = sf.read('speech1.wav')
@@ -13,6 +14,7 @@ from numpy.lib.stride_tricks import sliding_window_view
 # 2000-2400 haben wir uns angeguckt
 # wir haben mit dem Auge bestimmt: 70 samples zwischen den peaks
 # da die sampling rate 16000 ist, haben wir 70/16000 = 0.004375s
+# 1/0.004375 = 228.57, weil 1/periode = frequenz
 # IN Hz sind das 228.57Hz
 # -> female speaker (around 200hz)
 
@@ -28,7 +30,7 @@ data, samplerate = sf.read('speech2.wav')
 # sd.play(data, samplerate, blocking=True)
 
 #exercise 1.2
-def my_windowing ( v_signal : np . ndarray , sampling_rate : int , frame_length : int , frame_shift : int ):
+def my_windowing( v_signal : np . ndarray , sampling_rate : int , frame_length : int , frame_shift : int ):
     frame_length_to_seconds = frame_length/1000
     frame_length_to_ticks = int(frame_length_to_seconds * sampling_rate)
     frame_shift_to_seconds = frame_shift/1000
@@ -42,8 +44,8 @@ def my_windowing ( v_signal : np . ndarray , sampling_rate : int , frame_length 
 
 windows = my_windowing(data, samplerate, 1000, 500)
 
-for audio in windows:
-    sd.play(audio, samplerate, blocking=True)
+# for audio in windows:
+#     sd.play(audio, samplerate, blocking=True)
     
 def get_input_split_size(signal_length, sampling_rate, frame_length, frame_shift):
     frame_length_to_seconds = frame_length/1000
@@ -52,5 +54,33 @@ def get_input_split_size(signal_length, sampling_rate, frame_length, frame_shift
     frame_shift_to_ticks = int(frame_shift_to_seconds * sampling_rate)
     # wir gucken, wie viele frame_shifts in das singal passsen, aber müssen aufpassen, dass es gross genug ist für die frame_length_to_ticks
     return (signal_length - frame_length_to_ticks)//frame_shift_to_ticks
-    
+
+# exercise 1.3
+frames, v_time_frame = my_windowing(data, samplerate, 32, 16)
+
+def auto_correlate(v_signal_frame: np.ndarray):
+    return np.convolve(v_signal_frame, v_signal_frame[::-1], mode='full')/len(v_signal_frame)
+
+def auto_correlate_positive_values(v_signal_frame: np.ndarray):
+    auto_correlated_signal = auto_correlate(v_signal_frame)
+    return auto_correlated_signal[len(auto_correlated_signal)//2:]
+plt.plot(auto_correlate_positive_values(frames[10]))
+
+plt.show()
+
+vocal_sound = auto_correlate_positive_values(frames[10])
+
+
+# returns in HZ
+def get_fundamental_frequency(index, sampling_rate):
+    return 1/(index/sampling_rate)
+
+peaks = find_peaks(vocal_sound, height=0)[0]
+
+for peak in peaks:
+    print(get_fundamental_frequency(peak, samplerate))
+
+
+
+
     
