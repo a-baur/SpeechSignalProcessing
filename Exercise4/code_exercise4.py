754680b61c03c3bdf5c47a5ff026fa8deb247624
Valerie <v.bartel.vb@gmail.com>
2024-05-14 15:06:45 +0200

    start 4.2.2.2

--- Exercise4/code_exercise4.py
+++ Exercise4/code_exercise4.py
@@ -17,8 +17,26 @@ def load_audio(file_path: str) -> [np.ndarray, int]:
         - the sampling rate
     """
     return sf.read(file_path)
+def compute_power(signal_segment: np.ndarray) -> float:
+    """
+    Compute the power of a signal segment.
+
+    :param signal_segment: the signal segment
+    :return: the power of the signal segment
+    """
+    return np.sum(signal_segment**2)/len(signal_segment)
+
+def compute_power_for_all_frames(m_frames: np.ndarray) -> np.ndarray:
+    """
+    Compute the power for all frames.
+
+    :param m_frames: matrix containing the frames
+    :return: a vector containing the power for all frames
+    """
+    return np.apply_along_axis(compute_power, 1, m_frames)
 
 if __name__ == "__main__":
+    # 2.2.1
     x, fs = load_audio('Audio/female8khz.wav')
     N = 32
     R = 8 #segment shift
@@ -28,3 +46,14 @@ if __name__ == "__main__":
     # -> more detailed: we want to detect the different vowels and consonants in the signal?
     # Is a segment length of 32 ms appropriate? Why or why not?
     # -> convert_to_samples(32, fs) = 256 sampels (kann mir gerade nichts vorstellen)
+    
+    # 2.2.2
+    # Compute the power of each frame
+    v_power = compute_power_for_all_frames(m_frames)
+    # standrad deviation
+    v_std = np.sqrt(v_power)
+    # plot waveform and v_std
+    plt.plot(x)
+    plt.plot(v_time_frame, v_std)
+    plt.show()
+    # Why is it so short compared to the waveform? irgendwas ist komisch... also irgendwie aligned die zeit nicht
