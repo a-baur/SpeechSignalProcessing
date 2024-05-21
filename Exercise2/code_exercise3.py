155a6a20f2c91328a3e98454f2c1c9508c355231
Valerie <v.bartel.vb@gmail.com>
2024-05-21 12:36:59 +0200

    add answers to questions exercise 3

--- Exercise2/code_exercise3.py
+++ Exercise2/code_exercise3.py
@@ -9,10 +9,9 @@ import scipy as sp
 def compute_LP_coefficients(signal, M=12):
     R = np.correlate(signal, signal, mode='full')
     R = R[len(R)//2:]
-    R = -R
-    c = R[:M]
+    c = -R[:M]
     b = R[1:M+1]
-    a = sp.linalg.solve_toeplitz((c, c), b)
+    a = sp.linalg.solve_toeplitz(c, b)
     return a
     
     
@@ -26,17 +25,19 @@ def plot_frequency_response(a, fs, numPoints, whole=True):
     :param a: the LP coefficients
     :param fs: the sampling frequency
     """
-    w, h = sp.signal.freqz(1, a, numPoints, whole=whole, fs=fs)
+    w, h = sp.signal.freqz(1, np.concatenate(([1], a)), numPoints, whole=whole, fs=fs)
     plt.plot(w, abs(h)) # for the size use numPoints, the segment length in sampels
     plt.ylabel('Amplitude ')
     plt.xlabel('Frequency ')
+    plt.title("Frequency Response of the LP filter")
     plt.show()
     
 def plot_dft_and_filter(dft, a, fs, numPoints, gain=1):
-    w, h = sp.signal.freqz(gain, a, numPoints, whole=True, fs=fs)
+    w, h = sp.signal.freqz(gain, np.concatenate(([1], a)), numPoints, whole=True, fs=fs)
     h = h[:len(h)//2]
     plt.plot(20 * np.log10(np.abs(dft)), label='DFT')
     plt.plot(20 * np.log10(abs(h)), label='Filter')
+    plt.title(f"DFT and Filter with gain {gain}")
     plt.ylabel('Amplitude ')
     plt.xlabel('Frequency ')
     plt.legend()
@@ -44,13 +45,13 @@ def plot_dft_and_filter(dft, a, fs, numPoints, gain=1):
     plt.show()
 
 def plot_residual_signal(a, signal):
-    print(a)
-    a = np.concatenate(([1], a))
-    e = sp.signal.lfilter(a, 1, signal)
+    e = sp.signal.lfilter(np.concatenate(([1], a)), 1, signal)
+    print(np.sum(np.power(e, 2)))
     plt.plot(signal, label='Original Signal')
     plt.plot(e, label='Residual Signal')
     plt.legend()
-    plt.xlabel('Time')
+    plt.title('Residual Signal and Original Signal')
+    plt.xlabel('Time [in sampels]')
     plt.ylabel('Amplitude')
     plt.show()
 
@@ -58,6 +59,7 @@ def plot_pre_emphasized_signal_and_normal(signal, pre_emphasized):
     plt.plot(signal, label='Original Signal')
     plt.plot(pre_emphasized, label='Pre-emphasized Signal')
     plt.legend()
+    plt.title('Pre-emphasized Signal and Original Signal')
     plt.xlabel('Time')
     plt.ylabel('Amplitude')
     plt.show()
@@ -74,16 +76,22 @@ if __name__ == '__main__':
     voiced_segment = windows[10]
     unvoiced_segment = windows[33]
     
-    voiced_window = get_window('hann', get_index_from_time(32 / 1000, samplerate))
-    unvoiced_window = get_window('hann', get_index_from_time(32 / 1000, samplerate))
+    hann_window = get_window('hann', get_index_from_time(32 / 1000, samplerate))
+    
+    voiced_segment = voiced_segment * hann_window
+    unvoiced_segment = unvoiced_segment * hann_window
     
     # No. 3
     a_voiced = compute_LP_coefficients(voiced_segment)
     a_unvoiced = compute_LP_coefficients(unvoiced_segment)
+    print(a_voiced )
+    print(a_unvoiced)
     
     # No. 4
     # plot_frequency_response(a_voiced, samplerate, len(voiced_segment))
     # plot_frequency_response(a_unvoiced, samplerate, len(unvoiced_segment))
+    # b) Why do you use np.concatenate(([1], a)) and not only a?
+    # -> because the first element of the denominator is 1, so we need to add it to the array (a_0)
     
     # No. 5
     dft_voiced = np.fft.rfft(voiced_segment)
@@ -97,14 +105,25 @@ if __name__ == '__main__':
     # plot_residual_signal(a_voiced, voiced_segment)
     # plot_residual_signal(a_unvoiced, unvoiced_segment) 
     
+    # b) Explain differences in e between the voiced and unvoiced segment
+    # -> for voiced: low error, because it is a periodic signal, so prediction is easier; for unvoiced: higher error, because "random" signal is difficult to predict
+    # c) Explain why scipy.signal.lfilter(np.concatenate(([1], a)), 1, s) yields the residual signal
+    # ->  inverse of filter applied to signal should yield a constant, if it is approximately correct. Should it be 0 or a constant?
+    
     # No. 7
+    # a) Why are the logarithmic amplitudes of H and S (plots of assignment 5) not on the same level?
+    # -> because the gain is different, gain information is lost in the DFT
+    
+    # b) How can you modify H to achieve a better match? Hint: Experiment with the energy of the residual e
+    # -> See no correlation between residual energy and the gain of the filter
     
     # maybe modify the gain?
-    # plot_dft_and_filter(dft_voiced, a_voiced, samplerate, len(voiced_segment), gain=0.1)
-    # plot_dft_and_filter(dft_unvoiced, a_unvoiced, samplerate, len(unvoiced_segment), gain=0.1)
+    # plot_dft_and_filter(dft_voiced, a_voiced, samplerate, len(voiced_segment), gain=2.8312536210370037e-05)
+    # plot_dft_and_filter(dft_unvoiced, a_unvoiced, samplerate, len(unvoiced_segment), gain=0.00043334175528043006)
     
-    # plot_dft_and_filter(dft_voiced, a_voiced, samplerate, len(voiced_segment), gain=0.02)
-    # plot_dft_and_filter(dft_unvoiced, a_unvoiced, samplerate, len(unvoiced_segment), gain=0.02)
+    # square root of the energy of the residual signal
+    # plot_dft_and_filter(dft_voiced, a_voiced, samplerate, len(voiced_segment), gain=0.005320952)
+    # plot_dft_and_filter(dft_unvoiced, a_unvoiced, samplerate, len(unvoiced_segment), gain=0.0208)
     
     # No. 8
     
@@ -116,19 +135,27 @@ if __name__ == '__main__':
     # plot_dft_and_filter(dft_voiced, a_voiced_16, samplerate, len(voiced_segment))
     # plot_dft_and_filter(dft_voiced, a_voiced_32, samplerate, len(voiced_segment))
     
+    # Describe differences in H(z) and explain reasons for that
+    # -> differences: more detailed representation of the signal, because we can model the signal better with more coefficients (coefficient == blocks in block diagram)
+    
     # No. 9
     # b are the numerators
     # a are the denominators
     # alpha is 0.95
     
      
-    pre_emphasized_voiced = sp.signal.lfilter([1, -0.95], 1, voiced_segment)
-    pre_emphasized_unvoiced = sp.signal.lfilter([1, -0.95], 1, unvoiced_segment)
+    pre_emphasized_voiced = sp.signal.lfilter([1, 0.95], 1, voiced_segment)
+    pre_emphasized_unvoiced = sp.signal.lfilter([1, 0.95], 1, unvoiced_segment)
 
     plot_pre_emphasized_signal_and_normal(voiced_segment, pre_emphasized_voiced)
     plot_pre_emphasized_signal_and_normal(unvoiced_segment, pre_emphasized_unvoiced)
     
-    
+    # a) Compare the results with and without pre-emphasis.
+    # -> pre-emphasized singal makes high voice segments proportionally higher, so it is easier to detect them
+
+    # b) What is the advantage of pre-emphasizing the speech signal?
+    # -> makes it easier to detect high voice segments, making "noise" count less
+
     
     
     
